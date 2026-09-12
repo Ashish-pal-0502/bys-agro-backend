4c8bb5a746f7ec61575209e3cc90b3595b6a226f
Ashish Pal <ashishpal0502@gmail.com>
2026-09-12 14:19:30 +0530

    fix: add missing URL to CORS whitelist

--- middleware/config.js
+++ middleware/config.js
@@ -33,6 +33,8 @@ const whitelist = ['http://localhost:5000', 'http://localhost:5173', 'http://loc
   'https://bys-agro-frontend.onrender.com',
   'http://bys-agro-panel.s3-website.ap-south-1.amazonaws.com',
   'http://3.109.124.131:3000',
+  'https://main.dt06z443eijgj.amplifyapp.com',
+
   
 ]
 
